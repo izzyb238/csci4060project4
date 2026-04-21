cd5bc528288fc1dafe1a0d4534fb92b646383a3d
Karsen Phillips <kap87953@uga.edu>
2026-04-21 00:16:37 -0400

    Change layout from item_quiz_result to activity_main

--- app/src/main/java/edu/uga/cs/project4/MainActivity.java
+++ app/src/main/java/edu/uga/cs/project4/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.item_quiz_result);
+        setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
             showFragment(new SplashFragment(), false);
